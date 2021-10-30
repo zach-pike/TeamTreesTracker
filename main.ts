ce5c048c77281394a8f44247aea6694516db2673
Zachary Pike <ZachTurnPike@gmail.com>
2021-10-30 15:09:22 -0400

    initial commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+import fetch from "node-fetch"
+import ms from "ms"
+import fs from "fs/promises"
+
+setInterval(async () => {
+    let logfile = await fs.readFile("logfile.txt", "utf8");
+
+    let resp = await (await fetch("https://tscache.com/donation_total.json")).json() as { count: number }
+
+    logfile += `${Date.now()},${resp.count}\n`;
+
+    await fs.writeFile("logfile.txt", logfile);
+}, ms("10s"))
